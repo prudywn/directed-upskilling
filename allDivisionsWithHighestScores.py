a9651bf5d099003d7b730044b8cb3bb34a109cef
Prudence <162975278+prudywn@users.noreply.github.com>
2025-09-09 21:44:10 +0000

    implemented one which is faster

--- allDivisionsWithHighestScores.py
+++ allDivisionsWithHighestScores.py
@@ -1,21 +1,24 @@
 class Solution:
     def maxScoreIndices(self, nums: List[int]) -> List[int]:
         total_ones = sum(nums)
-        max_result = -1
+        max_score = -1
         result = []
         zeros = 0
         ones = 0
 
-        for i in range(len(nums) + 1):
+        for i in range(len(nums)+1):
             score = zeros + (total_ones - ones)
-            if score > max_result:
-                max_result = score
+            if score > max_score:
+                max_score = score
                 result = [i]
-            elif score == max_result:
+            elif score == max_score:
                 result.append(i)
 
             if i < len(nums):
                 if nums[i] == 0:
                     zeros += 1
-                elif nums[i] == 1:
+                else:
                     ones += 1
+        return result
+
+       
