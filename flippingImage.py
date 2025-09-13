76a6218137c6f379dea36ee34ea01e9d4de09b2b
Prudence <162975278+prudywn@users.noreply.github.com>
2025-09-13 21:32:08 +0000

    added comments

--- flippingImage.py
+++ flippingImage.py
@@ -1,6 +1,10 @@
 class Solution:
     def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
         for r in range(len(image)):
+            #flip and invert
             image[r] = [1 - val for val in image[r][::-1]]
+            """ 
+        [::-1] flips row.
+        1 - val inverts each element (1 → 0, 0 → 1). """
         
         return image
