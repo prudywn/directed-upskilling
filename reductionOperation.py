a5c359d5d5633f7817a3a0f9b42d2c48736bfb66
Prudence <162975278+prudywn@users.noreply.github.com>
2025-09-29 13:51:33 +0000

    reduction solution

---
+++ reductionOperation.py
@@ -0,0 +1,16 @@
+class Solution:
+    def reductionOperations(self, nums: List[int]) -> int:
+        nums.sort()
+        operations = 0
+        largest = nums[-1]
+        nextLargest = 0
+        n = len(nums)
+
+        # loop from right to left
+        for i in range(n -2, -1, -1):
+            if nums[i] < largest: #found the next smaller number
+                nextLargest = nums[i]
+                operations += n - i - 1 # all bigger numbers reduced once
+                largest = nextLargest
+        
+        return operations    
