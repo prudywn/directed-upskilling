5d023bab088978d98d5eeabf835e9373ba8730fe
Prudence <162975278+prudywn@users.noreply.github.com>
2025-10-12 20:15:57 +0000

    moving zeroes to the end using pointer

---
+++ moveZeroes.py
@@ -0,0 +1,18 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        insert_pos = 0
+
+        for num in nums:
+            if num != 0:
+                nums[insert_pos] = num
+                insert_pos += 1
+
+        for i in range(insert_pos, len(nums)):
+            nums[i] = 0
+
+                
+        
